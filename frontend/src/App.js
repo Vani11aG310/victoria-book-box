aea227af46f9f83cb076bc819900482f88280d35
rod-nickle <rod@bitwise-it.com>
2024-12-27 16:45:04 -0700

    Add Axios package. Allow CORS for Rails backend.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/api/wishlists/user_id/3")
+      .then((res) => console.log(res.data))
+      .catch((err) => {
+        if (err.response) {
+          console.error("Response error:", err.response);
+        } else if (err.request) {
+          console.error("Request error:", err.request);
+        } else {
+          console.error("General error:", err.message);
+        }
+      })
+      ;
+  }, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
-      <h1>Victoria Book Box</h1>
+        <h1>Victoria Book Box</h1>
       </header>
     </div>
   );
