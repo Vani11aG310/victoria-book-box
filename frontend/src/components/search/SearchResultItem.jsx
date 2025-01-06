9c08891d8601d3a8b2c0ae6ee69236f4a3e0ee32
rod-nickle <rod@bitwise-it.com>
2025-01-06 16:21:50 -0700

    Change + and - buttons to hearts.

--- frontend/src/components/search/SearchResultItem.jsx
+++ frontend/src/components/search/SearchResultItem.jsx
@@ -1,9 +1,10 @@
 import "../../styles/search/SearchResultItem.scss";
-import { FaPlusCircle } from "react-icons/fa";
+import { FaPlusCircle, FaHeart, FaRegHeart } from "react-icons/fa";
 import { useContext } from "react";
 import StateContext from "../../context/StateContext";
 import DispatchContext from "../../context/DispatchContext";
 import wishListDataCreate from "../../db/wishlists/wishlistDataCreate";
+import wishListDataDelete from "../../db/wishlists/wishlistDataDelete";
 
 const SearchResultItem = (props) => {
   const { book } = props;
@@ -15,25 +16,28 @@ const SearchResultItem = (props) => {
   const dispatch = useContext(DispatchContext);
 
   // Is the book already included in the wishlist?
-  // If yes, do not show the + button.
-  const isIncludedInWishlist = state.wishlistData.some((wishlistItem) => wishlistItem.book.open_library_key === book.key);
+  const wishlistItem = state.wishlistData.find((wishlistItem) => wishlistItem.book.open_library_key === book.key);
 
   const handleAdd = () => {
     wishListDataCreate({
-        userId, 
-        book: {
-          title: book.title,
-          author: book.author_name && book.author_name[0],
-          subject: book.subject && book.subject[0],
-          openLibraryKey: book.key,
-          openLibraryCoverKey: book.cover_edition_key,
-        },
+      userId,
+      book: {
+        title: book.title,
+        author: book.author_name && book.author_name[0],
+        subject: book.subject && book.subject[0],
+        openLibraryKey: book.key,
+        openLibraryCoverKey: book.cover_edition_key,
       },
+    },
       dispatch
     );
   }
 
-   return (
+  const handleDelete = () => {
+    wishListDataDelete(wishlistItem.id, dispatch);
+  }
+
+  return (
     <div className="wishlist__item">
       <img className="search-result__book-cover" src={book.cover_url} alt={`Cover picture of ${book.title}`} />
       <div className="search-result__book-info">
@@ -43,8 +47,12 @@ const SearchResultItem = (props) => {
         {/* <p className="search-result__book-open-library-key">Key: <strong>{book.key}</strong></p> */}
         {/* <p className="search-result__book-open-library-cover-key">Cover Key: <strong>{book.cover_edition_key}</strong></p> */}
       </div>
-      <div className="search-result__add-button">
-        {!isIncludedInWishlist && <FaPlusCircle className="search-result__add-icon" onClick={() => handleAdd()} />}
+      {/* <div className="search-result__add-button"> */}
+      {/* <FaPlusCircle className="search-result__add-icon" onClick={() => handleAdd()} /> */}
+      {/* </div> */}
+      <div className="search-result__heart-button">
+        {wishlistItem && <FaHeart className="search-result__heart-icon" onClick={() => handleDelete()} />}
+        {!wishlistItem && <FaRegHeart className="search-result__reg-heart-icon" onClick={() => handleAdd()} />}
       </div>
     </div>
   );
