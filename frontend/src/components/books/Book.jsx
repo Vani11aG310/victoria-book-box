4b0b882fd961fefd1882ca0d9ea7816af4bfba73
Angelo <angvil02@gmail.com>
2025-01-06 20:19:07 -0800

    book details now has access to book box location data from collections

--- frontend/src/components/books/Book.jsx
+++ frontend/src/components/books/Book.jsx
@@ -1,10 +1,17 @@
+import { useLocation } from "react-router-dom";
 import "../../styles/books/Book.scss";
 
 const Book = () => {
+  const location = useLocation();
+
+  const { collection, bookDetails } = location.state
+  console.log(bookDetails)
     
   return (
     <div>
-      <h4>This is a Book</h4>
+      <h2>{collection[0].book.title}</h2>
+      <img src={collection[0].book.cover_url} alt="book cover" />
+      <p>{bookDetails.book_description}</p>
     </div>
   );
 }
--- frontend/src/components/books/BooklistItem.jsx
+++ frontend/src/components/books/BooklistItem.jsx
@@ -1,19 +1,37 @@
 import "../../styles/books/BooklistItem.scss";
 import { useContext } from "react";
 import DispatchContext from "../../context/DispatchContext";
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const BooklistItem = (props) => {
   const { booklistItem } = props;
 
+  const [collection, setCollection] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios.get(`http://localhost:3001/api/collections?book_id=${booklistItem.id}`)
+    .then((res) => {
+      setCollection([...collection, res.data]);
+    })
+    .catch((err) => {
+      setError(err.message);
+      console.error("Error: ", err.message);
+    });
+  }, [booklistItem.id]);
+
+
   return (
-    <div className="booklist-item__item">
+    <Link className="booklist-item__item" to={{pathname:`/books/${booklistItem.id}`}} state={{ collection: collection[0], bookDetails: booklistItem }}>
       <img className="booklist-item__book-cover" src={booklistItem.cover_url} alt={`Cover picture of ${booklistItem.title}`} />
       <div className="booklist-item__book-info">
         <p className="booklist-item__book-title"><strong>{booklistItem.title}</strong></p>
         <p className="booklist-item__book-author">By: <strong>{booklistItem.author}</strong></p>
         <p className="booklist-item__book-subject">Subject: <strong>{booklistItem.subject}</strong></p>
       </div>
-    </div>
+    </Link>
   );
 }
 
