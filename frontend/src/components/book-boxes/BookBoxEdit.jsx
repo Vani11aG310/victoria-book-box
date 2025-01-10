727c2f380e0af127bb09fe3f180a8a12b6c0788a
rod-nickle <rod@bitwise-it.com>
2025-01-10 16:09:55 -0700

    Create Book Box

--- frontend/src/components/book-boxes/BookBoxEdit.jsx
+++ frontend/src/components/book-boxes/BookBoxEdit.jsx
@@ -1,63 +1,58 @@
 import "../../styles/book-boxes/BookBoxEdit.scss";
-import { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, useMemo } from "react";
+import { useParams } from "react-router-dom";
 import StateContext from "../../context/StateContext";
-import { useParams } from 'react-router-dom';
 import DispatchContext from "../../context/DispatchContext";
 import usePageTitle from "../../hooks/usePageTitle";
-import { ACTIONS } from "../../reducers/dataReducer";
 
 const BookBoxEdit = (props) => {
   let { mode } = props;
   const state = useContext(StateContext);
   const dispatch = useContext(DispatchContext);
-  let {id} = useParams();
+  let { id } = useParams();
   id = Number(id);
-  let bookBox = {};
-  let title = "";
 
+  const initialBookBox = useMemo(() => {
+    if (mode === "edit" && state.bookBoxes) {
+      const foundBookBox = state.bookBoxes?.find((bookBox) => bookBox.id === id);
+      if (foundBookBox) {
+        return foundBookBox;
+      }
+    }
+    return {
+      id: null,
+      name: "",
+      address: ""
+    };
+  }, [mode, state.bookBoxes, id]);
 
-  if (mode === "edit" && state.bookBoxes) {
-    bookBox = state.bookBoxes?.find((bookBox) => bookBox.id === id);
-    title = "Edit Book Box";
-  } else {
-    bookBox.id = null;
-    bookBox.name = null
-    bookBox.address = null;
-    title = "Create Book Box"
-  }
+  const [bookBox, setBookBox] = useState(initialBookBox);
+  const title = mode === "edit" ? "Edit Book Box" : "Create Book Box";
   usePageTitle(title, dispatch);
 
- 
-  // useEffect(() => {
-  //   setUserId(currentUserId);
-  // }, [currentUserId]);
- 
+  // Update bookBox state when initialBookBox changes
+  useEffect(() => {
+    setBookBox(initialBookBox);
+  }, [initialBookBox]);
 
   const handleChange = (event) => {
-    console.log(event.target.value);
+    const { name, value } = event.target;
+    setBookBox((prevBookBox) => ({
+      ...prevBookBox,
+      [name]: value
+    }));
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // const newUserId = Number(event.target.userId.value)
-    // setUserId(newUserId);
-
-    // // Update the global state with the new User.
-    // const newUser = users.find((user) => user.id === newUserId);
-    // dispatch({
-    //   type: ACTIONS.SET_USER,
-    //   payload: newUser,
-    // })
-
-    // // If the userId is changed, we need to refresh the Wishlist.
-    // wishlistDataFetch(newUserId, dispatch);
-  }
+    // Handle form submission logic here
+  };
 
   const handleCancel = (event) => {
-
+    // Handle form cancel logic here
   }
 
-  if (!bookBox) {
+  if (!bookBox || (mode === "edit" && bookBox.id ===null)) {
     return (
       <div>
         <h3>{`Book Box ${id} not found.`}</h3>
@@ -69,24 +64,24 @@ const BookBoxEdit = (props) => {
     <div>
       <form className="book-box__form" onSubmit={handleSubmit}>
         <div className="book-box__input-group">
-          <label htmlFor="name" className="book-box__label">Name</label>
+          <label htmlFor="name" className="book-box__label">Name:</label>
           <input
-            name="name" 
+            name="name"
             className="book-box__input"
             autoComplete="off"
-            value={bookBox.name} 
+            value={bookBox.name}
             onChange={handleChange}
-            />
+          />
         </div>
         <div className="book-box__input-group">
-          <label htmlFor="address" className="book-box__label">Address</label>
+          <label htmlFor="address" className="book-box__label">Address:</label>
           <input
-            name="address" 
+            name="address"
             className="book-box__input"
             autoComplete="off"
-            value={bookBox.address} 
+            value={bookBox.address}
             onChange={handleChange}
-            />
+          />
         </div>
         <div className="book-box__button-group">
           <button 
@@ -102,6 +97,6 @@ const BookBoxEdit = (props) => {
       </form>
     </div>
   );
-}
+};
 
 export default BookBoxEdit;
