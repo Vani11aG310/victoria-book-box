1b404a750e3c429a5b4c48b2e3909a41caae4c21
MPNose <mpnoseworthy89@gmail.com>
2025-01-11 12:45:11 -0800

    add media adjustment styling for wishlist, book component

--- frontend/src/components/books/Book.jsx
+++ frontend/src/components/books/Book.jsx
@@ -4,6 +4,7 @@ import "../../styles/book-boxes/BookBoxList.scss";
 import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import StateContext from "../../context/StateContext";
+import { PiBooks } from "react-icons/pi";
 
 const Book = () => {
   const state = useContext(StateContext)
@@ -21,46 +22,52 @@ const Book = () => {
 
   useEffect(() => {
     axios.get(`http://localhost:3001/api/collections?book_id=${bookId}`)
-    .then((res) => {
-      setCollection(res.data);
-      axios.get(`http://localhost:3001/api/books/${bookId}`)
       .then((res) => {
-        setBookDetails(res.data);
-        setLoading(false);
+        setCollection(res.data);
+        axios.get(`http://localhost:3001/api/books/${bookId}`)
+          .then((res) => {
+            setBookDetails(res.data);
+            setLoading(false);
+          })
+          .catch((err) => {
+            setError(err.message);
+            console.error("Error: ", error);
+          });
       })
       .catch((err) => {
         setError(err.message);
         console.error("Error: ", error);
       });
-    })
-    .catch((err) => {
-      setError(err.message);
-      console.error("Error: ", error);
-    });
   }, [bookId]);
-  
-    
+
+
   return (
     <div>
       {loading && <h2>Loading Book Details...</h2>}
       {!loading && <h2 className="book__title">{bookDetails.title}</h2>}
-      {!loading &&<h3 className="book__author">By: {bookDetails.author}</h3>}
-      {!loading &&<h3 className="book__author">Subject: {bookDetails.subject} </h3>}
-      {!loading &&<img src={bookDetails.cover_url} alt="book cover" className="book__cover" />}
+      {!loading && <h3 className="book__author">By: {bookDetails.author}</h3>}
+      {!loading && <h3 className="book__author">Subject: {bookDetails.subject} </h3>}
+      {!loading && <img src={bookDetails.cover_url} alt="book cover" className="book__cover" />}
       {!loading && <p>{bookDetails.book_description}</p>}
-      {!loading &&<h3>Book Available At:</h3>}
-      {!loading && <ul className="book__info-list">
-                      {!loading && Array.isArray(collection) && collection.map((item) => (
-                        <Link to={`/book-boxes/${!loading && item.book_box.id}`} state={{ bookBox: item.book_box}}>
-                            <li className="book-boxes__item">
-                              <div className="book-boxes__content">
-                                <h4 className="book-boxes__name">{item.book_box.name}</h4>
-                                <p className="book-boxes__address">{item.book_box.address}</p>
-                              </div>
-                            </li>
-                        </Link>
-                      ))}
-                  </ul>}
+      {!loading && <h3>Book Available At:</h3>}
+      {!loading && (
+        <ul
+          className={`book__info-list ${collection && collection.length === 1 ? "single-item" : "multi-item"
+            }`}
+        >
+          {!loading && Array.isArray(collection) && collection.map((item) => (
+            <Link to={`/book-boxes/${!loading && item.book_box.id}`} state={{ bookBox: item.book_box }}>
+              <li className="book-boxes__item">
+                  <PiBooks className="book-boxes__book-icon" />
+                <div className="book-boxes__content">
+                  <h4 className="book-boxes__name">{item.book_box.name}</h4>
+                  <p className="book-boxes__address">{item.book_box.address}</p>
+                </div>
+              </li>
+            </Link>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
