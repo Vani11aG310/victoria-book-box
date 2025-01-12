0b309a833d99649442632e02df82384eeb5c03b2
rod-nickle <rod@bitwise-it.com>
2025-01-11 17:00:35 -0700

    Add link to Book Details from Wishlist.

--- frontend/src/components/wishlists/WishlistItem.jsx
+++ frontend/src/components/wishlists/WishlistItem.jsx
@@ -1,6 +1,7 @@
 import "../../styles/wishlists/WishlistItem.scss";
 import { FaHeart } from "react-icons/fa";
 import { useContext } from "react";
+import { Link } from 'react-router-dom';
 import DispatchContext from "../../context/DispatchContext";
 import wishListDataDelete from "../../db/wishlists/wishlistDataDelete";
 
@@ -15,7 +16,9 @@ const WishlistItem = (props) => {
 
   return (
     <div className="wishlist__item">
-      <img className="wishlist__book-cover" src={wishlistItem.book.cover_url} alt={`Cover picture of ${wishlistItem.book.title}`} />
+      <Link to={{ pathname: `/books/${wishlistItem.book_id}` }} state={{ bookId: wishlistItem.book_id }}>
+        <img className="wishlist__book-cover" src={wishlistItem.book.cover_url} alt={`Cover picture of ${wishlistItem.book.title}`} />
+      </Link>
       <div className="wishlist__book-info">
         <p className="wishlist__book-title"><strong>{wishlistItem.book.title}</strong></p>
         <p className="wishlist__book-author">By: <strong>{wishlistItem.book.author}</strong></p>
