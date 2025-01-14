911a594a405046f802a8f6355aa81a899c9cb347
Angelo <angvil02@gmail.com>
2025-01-13 19:18:03 -0800

    Create submission status for add a book to box

--- frontend/src/components/search/SearchResult.jsx
+++ frontend/src/components/search/SearchResult.jsx
@@ -1,12 +1,27 @@
 import "../../styles/search/SearchResult.scss";
 import SearchResultItem from "./SearchResultItem";
 import useSearchOpenLibrary from "../../hooks/useSearchOpenLibrary";
+import { useState } from "react";
 
 const SearchResult = (props) => {
   const { searchValue, mode, boxId } = props;
 
   // Use the custom hook to search Open Library for books by title or author.
   const { books, loading, error } = useSearchOpenLibrary(searchValue);
+
+  const [submitting, setSubmitting] = useState(false);
+
+  const submission = (submit) => {
+    setSubmitting(submit);
+  }
+
+  if (submitting) {
+    return (
+      <div>
+        <h3 className="search-result__status">Submitting...</h3>
+      </div>
+    )
+  }
 
   // Loading component.
   if (loading) {
@@ -31,7 +46,7 @@ const SearchResult = (props) => {
     <div>
       <ul className="search-result">
         {Array.isArray(books) && books.map((book) => {
-          return <SearchResultItem key={book.key} book={book} mode={mode} boxId={boxId}/>;
+          return <SearchResultItem key={book.key} book={book} mode={mode} boxId={boxId} setSubmit={submission}/>;
         })}
       </ul>
     </div>
